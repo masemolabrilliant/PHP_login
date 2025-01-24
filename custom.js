3612cfc03484dce1c3e78dc20584d0b84435ca4e
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-01-24 11:03:52 +0200

    js

---
+++ custom.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function  checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ""){
+            showError(input, `${getFieldName(input) is Required}`);
+        } 
+        else { 
+            showSuccess(input);
+        }
+    });
+}
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+});
